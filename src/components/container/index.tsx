899395fc04c7ee7e68fd03aca6c15cbbd243776b
josuecb <josuecb@monopolio.io>
2026-04-02 17:18:57 -0400

    my dynamic

---
+++ src/components/container/index.tsx
@@ -0,0 +1,19 @@
+import { Stack, Container, StackProps } from "@chakra-ui/react"
+import { ReactNode } from "react"
+
+type Props = {
+    children: ReactNode
+} & StackProps
+
+export const ContainerLanding = ({
+    children,
+    ...props
+}: Props) => {
+    return (
+        <Stack align={'center'} bg="#F3A0FF0D" minH={'100vh'} justifyContent={'center'} py={6} {...props} pt={{ base: 20, md: 0 }}>
+            <Container maxW={{ base: 'sm', md: '100%' }} px={{ base: 6, md: 40 }}>
+                {children}
+            </Container>
+        </Stack>
+    )
+}
