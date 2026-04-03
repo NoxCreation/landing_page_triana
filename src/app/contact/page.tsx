62bb7b1c3fb01f9e0c54562915bb8bf7d97bf915
josuecb <josuecb@monopolio.io>
2026-04-03 12:07:05 -0400

    contact dynamic

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,14 @@
+import { getContent } from "@/lib/content";
 import ContactIndex from "@/modules/contact";
+import { redirect } from 'next/navigation';
 
-export default function Contact() {
+export const dynamic = 'force-dynamic';
+
+export default async function Contact() {
+    let content = await getContent()
+    if (!content) redirect("/500")
+
     return (
-        <ContactIndex/>
+        <ContactIndex content={content} />
     )
 }
