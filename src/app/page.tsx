acd76b414768143e6e0cbacc6e2f3916c9d4ce65
josuecb <josuecb@monopolio.io>
2026-04-03 23:13:36 -0400

    testing 2 metada ceo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,12 @@
-import LayoutCEO from "@/components/LayoutCEO";
 import { getContent } from "@/lib/content";
+import { geMetadata } from "@/lib/geMetadata";
 import HomeIndex from "@/modules/home";
 import { redirect } from 'next/navigation';
 
 export const dynamic = 'force-dynamic';
 
-// Genera los metadatos en el servidor (óptimo para SEO)
-export async function generateMetadata(): Promise<any> {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://3anamarketing.com';
-  const title = "3ana Marketing & Consulting";
-  const description = "Agencia de marketing digital especializada en SEO, SEM, redes sociales y diseño web. Impulsa tu negocio con estrategias personalizadas y resultados medibles.";
-  const image = "/social.webp";
-  const fullImageUrl = image.startsWith('http') ? image : `${baseUrl}${image}`;
-
-  return {
-    title,
-    description,
-    openGraph: {
-      title,
-      description,
-      url: baseUrl,
-      type: 'website',
-      images: [{ url: fullImageUrl }],
-    },
-    twitter: {
-      card: 'summary_large_image',
-      title,
-      description,
-      images: [fullImageUrl],
-    },
-  };
+export async function generateMetadata() {
+    return geMetadata()
 }
 
 export default async function Home() {
