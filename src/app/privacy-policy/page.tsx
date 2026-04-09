c5421476b18d279315b787fd65cbce3c7d4db81f
josuecb <josuecb@monopolio.io>
2026-04-08 21:51:24 -0400

    read privacy policy

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,30 @@
+import { getPrivacyPolicyContent } from "@/lib/content";
+import { getMetadata } from "@/lib/getMetadata";
+import PrivacyPolicyIndex from "@/modules/privacy-policy";
+import { redirect } from 'next/navigation';
+
+export const dynamic = 'force-dynamic';
+
+export async function generateMetadata() {
+    return getMetadata({
+        title: "Política de Privacidad - Triana Marketing",
+        description: "Política de privacidad de Triana Marketing. Conoce cómo protegemos tus datos personales y garantizamos tu privacidad en nuestros servicios de marketing digital.",
+        keywords: [
+            "Política de Privacidad",
+            "Triana Marketing",
+            "Protección de Datos",
+            "Privacidad en Marketing Digital",
+            "Seguridad de la Información",
+            "Confidencialidad",
+        ]
+    })
+}
+
+export default async function Home() {
+    let content = await getPrivacyPolicyContent()
+    if (!content) redirect("/500")
+
+    return (
+        <PrivacyPolicyIndex content={content}/>
+    );
+}
